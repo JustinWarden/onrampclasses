abebe7f773aa87c435553191bff1db5b4db830f1
Newforce39 <justin.warden@outlook.com>
2019-07-15 18:16:06 -0400

    odin

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-alert ("hello world");
+// // alert ("hello world");
 
-var myName = "justin warden";
+// // var myName = "justin warden";
 
-console.log(myName);
+// // console.log(myName);
 
-var totalCost;
-var totalWithTax;
+// // var totalCost;
+// // var totalWithTax;
 
-var costPerItem = 2.50;
-var numberOfItems = 4;
-var taxRate = 0.06;
+// // var costPerItem = 2.50;
+// // var numberOfItems = 4;
+// // var taxRate = 0.06;
  
-totalCost = costPerItem * numberOfItems;
-totalWithTax = totalCost + (taxRate * totalCost);
+// // totalCost = costPerItem * numberOfItems;
+// // totalWithTax = totalCost + (taxRate * totalCost);
 
-console.log(totalWithTax);
+// // console.log(totalWithTax);
+
+
+
+// Declare a constant variable max with the value 57
+// Set another variable actual to max - 13
+// Set another variable percentage to actual / max
+// If you type percentage in the console and press enter you should see a value like 0.7719
+
+// const max=10;
+// var act=50;
+// var percent= act / max;
+// console.log(percent);
+// function add(a,b){
+//     return (a * b);
+// }
+// console.log(add(10,10));
+// const sumArray = function(array) {
+//     let sum = 0;
+//     array.forEach(function(number) {
+//       sum += number;
+//     });
+//     return sum;
+//   };
+  
+//   console.log (sumArray([2, 2, 2]));
+
+
+
+
 
